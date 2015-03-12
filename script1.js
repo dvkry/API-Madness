f576a291b83b9148c85a2f2ef4aaa5f845c912f7
David Vickery <david.vickery.23@gmail.com>
2015-03-12 00:37:41 +0000

    more features

--- script1.js
+++ script1.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
   $('#find_city_button').on('click', function() {
     var query = $("#query").val();
     // console.log(query);
-    $('#image_div').hide();
-    var wunderground_api = 'http://autocomplete.wunderground.com/aq?query=' + query
+    // $('#image_div').hide();
+    var wunderground_api = 'http://autocomplete.wunderground.com/aq?query=' + query;
     // console.log(wunderground_api);
 
     $.ajax({
@@ -21,15 +21,22 @@ $(document).ready(function() {
         var $data = $('#data')
         results = response.RESULTS
         // console.log(results);
-        var elements = $('<div>')
+        var elements = $('<div class="cities">')
 
         for (i = 0; i < results.length; i++) {
-          elements.append($('<div>').addClass('cityname'));
-          html_string = '<a href="http://www.wunderground.com/' + results[i].l + '">' + results[i].name + '</a>'
-          elements.append(html_string);
-          $data.html(elements);
+          element = $('<div class="cityname">');
+          $('<a href="http://www.wunderground.com/' + results[i].l + '">' + results[i].name + '</a>').appendTo(element);
+          elements.append(element);
         }
+        $data.html(elements);
       }
     });
   });
+
+  $(document.body).on('mouseover', '.cityname' , function() {
+    var name = $(this).text();
+    $('#big-city-name').text(name);
+    console.log($(name).text());
+  });
+
 });
